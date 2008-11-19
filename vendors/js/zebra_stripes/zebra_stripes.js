0e86e4ca2d30f5ed79ec9e1c28b9a34f339781fb
Jason Miyashiro <ippatsu.yarou@gmail.com>
2008-11-19 17:11:50 +0900

    adding support for dl/dd/dt using zebra stripes

--- vendors/js/zebra_stripes/zebra_stripes.js
+++ vendors/js/zebra_stripes/zebra_stripes.js
@@ -2,6 +2,7 @@
 $(document).ready(function()
 {
 	stripeTable();
+	stripeDl();
 });
 
 function stripeTable(table_name, class_name)
@@ -18,4 +19,20 @@ function stripeTable(table_name, class_name)
 		target = "table tr:even"; 
 		$(target).addClass("even-row");
 	}
+}
+
+function stripeDl(table_name, class_name)
+{
+    var target;
+
+    if(table_name && class_name)
+    {
+        target = table_name + " dt:even"; 
+        $(target).addClass(class_name);
+    }
+    else
+    {
+        target = "dt:even"; 
+        $(target).addClass("even-row");
+    }
 }
